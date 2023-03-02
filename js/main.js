35e74c7e519f327d5ca09003b4102e0a85a6787d
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-02 22:41:09 +0100

    select-html-css

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 */
 
 /*
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+*/
+
+/*
 ----------
 Functions
 ----------
